43d6e26b3fc29e8d56241fd2b21ac5b32d0dcdd4
Helena <144545068+Helena0509@users.noreply.github.com>
2024-04-30 09:32:18 +0200

    Update Doctrine.sql

--- Doctrine.sql
+++ Doctrine.sql
@@ -2,18 +2,20 @@ CREATE DATABASE rps_tournament;
 
 USE rps_tournament;
 
-CREATE TABLE game_rounds (
-                             id INT AUTO_INCREMENT PRIMARY KEY,
-                             player VARCHAR(255) NOT NULL,
-                             symbol ENUM('rock', 'paper', 'scissors') NOT NULL,
-                             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+DROP table game_rounds;
+CREATE TABLE game_rounds
+(
+    id         INT AUTO_INCREMENT PRIMARY KEY,
+    player     VARCHAR(255)                       NOT NULL,
+    symbol     ENUM ('rock', 'paper', 'scissors') NOT NULL,
+    player2    VARCHAR(255)                       NOT NULL,
+    symbol2    ENUM ('rock', 'paper', 'scissors') NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO game_rounds (player, symbol)
-VALUES
-    ('John Doe', 'rock'),
-    ('Jane Smith', 'paper'),
-    ('Mike Johnson', 'scissors'),
-    ('Sarah Lee', 'rock'),
-    ('William Brown', 'paper'),
-    ('William Browsn', 'paper');
+INSERT INTO game_rounds (id, player, symbol, player2, symbol2)
+VALUES (1,'John Doe', 'rock', 'Jane Smith', 'paper'),
+       (2,'Mike Johnson', 'scissors', 'Sarah Lee', 'rock'),
+       (3,'William Brown', 'paper', 'William Browsn', 'paper'),
+       (4,'Sarah Lee', 'rock', 'William Brown', 'paper'),
+       (5,'William Brown', 'paper', 'Sarah Lee', 'rock');
